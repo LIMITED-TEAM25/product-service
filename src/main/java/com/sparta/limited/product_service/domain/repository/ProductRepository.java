9b9af67280ee834390b076e721eecadede0962eb
sohyun119 <sh-119@naver.com>
2025-04-13 00:11:56 +0900

    ✨ feat [#2] : 상품 단건 조회 repository 구현

--- src/main/java/com/sparta/limited/product_service/domain/repository/ProductRepository.java
+++ src/main/java/com/sparta/limited/product_service/domain/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.sparta.limited.product_service.domain.repository;
 
 import com.sparta.limited.product_service.domain.model.Product;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface ProductRepository {
 
     void save(Product product);
 
+    Optional<Product> findByIdAndDeletedAtIsNull(UUID productId);
 }
