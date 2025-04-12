c8108e8a89ab5f4ff42855dd1fcea09a835e391d
sohyun119 <sh-119@naver.com>
2025-04-12 22:45:27 +0900

    ♻️ refactor [#6] : interface에서 Repository 어노테이션 제거

--- src/main/java/com/sparta/limited/product_service/infrastructure/persistence/JpaProductRepository.java
+++ src/main/java/com/sparta/limited/product_service/infrastructure/persistence/JpaProductRepository.java
@@ -3,9 +3,7 @@ package com.sparta.limited.product_service.infrastructure.persistence;
 import com.sparta.limited.product_service.domain.model.Product;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface JpaProductRepository extends JpaRepository<Product, UUID> {
 
 }
