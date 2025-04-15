586afb736d0b906fc73a5556799482e3246ac4dc
sohyun119 <sh-119@naver.com>
2025-04-15 22:14:14 +0900

    ✏️ docs [#12] : 공통모듈 aop스캔을 위한 어노테이션 추가 및 RoleCheck 어노테이션 추가

--- src/main/java/com/sparta/limited/product_service/ProductServiceApplication.java
+++ src/main/java/com/sparta/limited/product_service/ProductServiceApplication.java
@@ -3,9 +3,14 @@ package com.sparta.limited.product_service;
 import com.sparta.limited.common_module.common.EnableCommonModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCommonModule
+@ComponentScan(basePackages = {
+    "com.sparta.limited.product_service",
+    "com.sparta.limited.common_module.common.aop"
+})
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sparta/limited/product_service/presentation/controller/ProductController.java
+++ src/main/java/com/sparta/limited/product_service/presentation/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.sparta.limited.product_service.presentation.controller;
 
+import com.sparta.limited.common_module.common.aop.RoleCheck;
 import com.sparta.limited.product_service.application.dto.request.ProductCreateRequest;
 import com.sparta.limited.product_service.application.dto.response.ProductCreateResponse;
 import com.sparta.limited.product_service.application.dto.response.ProductReadResponse;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +24,11 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @RoleCheck("ADMIN")
     @PostMapping
     public ResponseEntity<ProductCreateResponse> createProduct(
-        @RequestBody ProductCreateRequest request) {
+        @RequestBody ProductCreateRequest request,
+        @RequestHeader("X-User-Id") Long userId) {
         ProductCreateResponse response = productService.createProduct(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
