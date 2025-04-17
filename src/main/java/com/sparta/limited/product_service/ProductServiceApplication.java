e342ff9e64a26ffa5a94851d6725cb83cabf59c4
sohyun119 <sh-119@naver.com>
2025-04-17 09:09:06 +0900

    ✨ feat [#12] : EnableFeignClients 어노테이션 추가

--- src/main/java/com/sparta/limited/product_service/ProductServiceApplication.java
+++ src/main/java/com/sparta/limited/product_service/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.sparta.limited.product_service;
 import com.sparta.limited.common_module.common.EnableCommonModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableCommonModule
 @ComponentScan(basePackages = {
     "com.sparta.limited.product_service",
