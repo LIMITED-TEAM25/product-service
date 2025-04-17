c9680ce4b9f782c2eefa5a67176fc19186bb1e14
sohyun119 <sh-119@naver.com>
2025-04-17 09:07:47 +0900

    ✨ feat [#12] : Product Update Mapper toUpdateReponse() 구현

--- src/main/java/com/sparta/limited/product_service/application/mapper/ProductMapper.java
+++ src/main/java/com/sparta/limited/product_service/application/mapper/ProductMapper.java
@@ -3,6 +3,8 @@ package com.sparta.limited.product_service.application.mapper;
 import com.sparta.limited.product_service.application.dto.request.ProductCreateRequest;
 import com.sparta.limited.product_service.application.dto.response.ProductCreateResponse;
 import com.sparta.limited.product_service.application.dto.response.ProductReadResponse;
+import com.sparta.limited.product_service.application.dto.response.ProductUpdateResponse;
+import com.sparta.limited.product_service.application.service.ProductClientUpdateInfo;
 import com.sparta.limited.product_service.domain.model.Product;
 
 public class ProductMapper {
@@ -19,6 +21,15 @@ public class ProductMapper {
     public static ProductReadResponse toReadResponse(Product product) {
         return ProductReadResponse.of(product.getId(), product.getTitle(),
             product.getDescription(), product.getPrice());
+    }
+
+    public static ProductUpdateResponse toUpdateResponse(Product product,
+        ProductClientUpdateInfo limitedProductResultMessage,
+        ProductClientUpdateInfo auctionProductResultMessage,
+        ProductClientUpdateInfo preuserProductResultMessage) {
+        return ProductUpdateResponse.of(product.getId(), product.getDescription(),
+            product.getPrice(), limitedProductResultMessage, auctionProductResultMessage,
+            preuserProductResultMessage);
     }
 
 }
