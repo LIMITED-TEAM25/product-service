1ebffd600da1f073fe9a04484bda96338baae38b
sohyun119 <sh-119@naver.com>
2025-04-22 01:09:03 +0900

    🐳 docker [#20] : 도커에 올리기 위해 오류사항 수정 - application.yml : profiles.active 설정 - application-test.yml : 생성 - datasource & jpa : config.activate.on-profile: local 붙여쓰기 - ProductServiceApplicationTests : 어노테이션 삭제 - docker-compose 파일 오타 수정

--- src/test/java/com/sparta/limited/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/sparta/limited/product_service/ProductServiceApplicationTests.java
@@ -2,9 +2,7 @@ package com.sparta.limited.product_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class ProductServiceApplicationTests {
 
